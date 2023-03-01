8923462857dc843afa68fd51acc21d6f512f92e6
Sitkevich88 <Sitkevich2002@mail.ru>
2023-03-01 13:56:34 +0300

    Изменил ArcCos.java

--- src/main/java/ArcCos.java
+++ src/main/java/ArcCos.java
@@ -2,26 +2,25 @@ public class ArcCos {
 
 
     public static double run(double x) {
-        if (x > 1 || x < -1) {
+        if (x > 1 || x < -1)
             return Double.NaN;
-        }
 
         double numeratorCoeff = 1;
         double denominatorCoeff = 2;
 
-        double e = 1;
-        double n = 2;
+        double e;
+        double n = 1;
         double arcsin = x;
 
-        while (Math.abs(e) > 0.0001) {
+        do {
+            numeratorCoeff *= 2 * n - 1;
+            denominatorCoeff *= 2 * n;
             e = (numeratorCoeff * Math.pow(x, 2 * n - 1)) / (denominatorCoeff * (2 * n - 1));
-            numeratorCoeff = numeratorCoeff * (2 * n - 1);
-            denominatorCoeff = denominatorCoeff * 2 * n;
-            arcsin = arcsin + e;
+            arcsin += e;
             n++;
-        }
+        }while (Math.abs(e) > 0.0001);
 
-        return Math.PI /2 -  arcsin;
+        return Math.PI / 2 -  arcsin;
     }
 
 
